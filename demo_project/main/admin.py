a9d50932fdb3a91e4269dfb7e6f5d61c757ba9aa
ravinalawade <ravinalawade5@gmail.com>
2021-03-20 16:56:15 +0530

    demo project done

--- demo_project/main/admin.py
+++ demo_project/main/admin.py
@@ -3,3 +3,22 @@ from .models import *
 
 # Register your models here.
 admin.site.register(Email)
+admin.site.register(Forest_employee)
+##COPY###
+# admin.site.register(Login)
+admin.site.register(Animal)
+admin.site.register(Camera)
+admin.site.register(Tasks)
+admin.site.register(Division_tasks)
+admin.site.register(Range_tasks)
+admin.site.register(Beat_tasks)
+admin.site.register(Logs)
+admin.site.register(Status)
+# admin.site.register(Researcher)
+# admin.site.register(Forest_employee)
+admin.site.register(Task_Description)
+admin.site.register(Report)
+admin.site.register(Division_range)
+admin.site.register(Range_beat)
+admin.site.register(Beat_employee)
+admin.site.register(Local_report)
